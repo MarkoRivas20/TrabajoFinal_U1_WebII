ce060d50642644944ea32607a282ab6a77a154df
Marko Rivas <48537799+MarkoRivas20@users.noreply.github.com>
2019-09-30 23:56:04 -0500

    Ejercicios

---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrabajoFinal_U1_WebII.MvcApplication" Language="C#" %>
---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Models/ClsEjercicio2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrabajoFinal_U1_WebII.Models
+{
+    public class ClsEjercicio2
+    {
+        public int fil { get; set; }
+        public int col { get; set; }
+    }
+}
