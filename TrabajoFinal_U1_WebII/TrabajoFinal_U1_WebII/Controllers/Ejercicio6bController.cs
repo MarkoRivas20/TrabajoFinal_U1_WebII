839a951d5d0fb6bebfa017e92cad52011af7a707
Andre Reinoso Aranda <37244658+Andre-Reinoso@users.noreply.github.com>
2019-10-01 11:46:32 -0500

    repositorio descomprimido
    repositorio descomprimido

---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Controllers/Ejercicio6bController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabajoFinal_U1_WebII.Controllers
+{
+    public class Ejercicio6bController : Controller
+    {
+        // GET: Ejercicio3
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public FileResult TasaMora()
+        {
+            if (Request.Form["Generar1"] != null)
+            {
+                var ruta = Server.MapPath("~/Documentos/DIRECTIVA_2019_NUEVA_TASA_MORA.pdf");
+                return File(ruta, "application/pdf", "DIRECTIVA_2019_NUEVA_TASA_MORA.pdf");
+            }
+            if (Request.Form["Generar2"] != null)
+            {
+                var ruta = Server.MapPath("~/Documentos/DIRECTIVA_2019_COBRANZAS.pdf");
+                return File(ruta, "application/pdf", "DIRECTIVA_2019_COBRANZAS.pdf");
+            }
+            if (Request.Form["Generar3"] != null)
+            {
+                var ruta = Server.MapPath("~/Documentos/reglamento_general_upt.pdf");
+                return File(ruta, "application/pdf", "reglamento_general_upt.pdf");
+            }
+            if (Request.Form["Generar4"] != null)
+            {
+                var ruta = Server.MapPath("~/Documentos/estatuto.pdf");
+                return File(ruta, "application/pdf", "estatuto.pdf");
+            }
+            if (Request.Form["Generar5"] != null)
+            {
+                var ruta = Server.MapPath("~/Documentos/REGLAMENTO_MATRICULA_ESTUDIOSYEVALUACION.pdf");
+                return File(ruta, "application/pdf", "REGLAMENTO_MATRICULA_ESTUDIOSYEVALUACION.pdf");
+            }
+            if (Request.Form["Generar6"] != null)
+            {
+                var ruta = Server.MapPath("~/Documentos/REGLAMENTO_GRADOS-TITULOS_AGOSTO_2015.pdf");
+                return File(ruta, "application/pdf", "REGLAMENTO_GRADOS-TITULOS_AGOSTO_2015.pdf");
+            }
+            if (Request.Form["Generar7"] != null)
+            {
+                var ruta = Server.MapPath("~/Documentos/REGLAMENTO_DIPLOMADO_ POSTGRADO_setiembre_2015.pdf");
+                return File(ruta, "application/pdf", "REGLAMENTO_DIPLOMADO_ POSTGRADO_setiembre_2015.pdf");
+            }
+            if (Request.Form["Generar8"] != null)
+            {
+                //return File("~/Documentos/EXPEDICION_DOCUMENTOS_OFICIALES_EN_SECRETARIA_GENERAL_2015.pdf", "application/pdf");
+                var ruta = Server.MapPath("~/Documentos/EXPEDICION_DOCUMENTOS_OFICIALES_EN_SECRETARIA_GENERAL_2015.pdf");
+                return File(ruta, "application/pdf", "EXPEDICION_DOCUMENTOS_OFICIALES_EN_SECRETARIA_GENERAL_2015.pdf");
+            }
+            else
+            {
+                var ruta = Server.MapPath("~/Documentos/DIRECTIVA_2019_NUEVA_TASA_MORA.pdf");
+                return File(ruta, "application/pdf", "DIRECTIVA_2019_NUEVA_TASA_MORA.pdf");
+            }
+
+
+        }
+    }
+}
---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Models/ClsEjercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrabajoFinal_U1_WebII.Models
+{
+    public class ClsEjercicio1
+    {
+        public int[,] matriz { get; set; } = new int[3, 3];
+        public double promedio { get; set; }
+
+        public int mayor { get; set; }
+        public int menor { get; set; }
+        public int diagonalprincipal { get; set; }
+        public int diagonalsecundaria { get; set; }
+        public int valoresencima { get; set; }
+        public int valoresdebajo { get; set; }
+    }
+}
