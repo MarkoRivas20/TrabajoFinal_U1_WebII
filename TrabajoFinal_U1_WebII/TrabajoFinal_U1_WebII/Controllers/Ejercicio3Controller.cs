3230d1b2ce718bb02c89e63c7ddb05dc190db441
Andre Reinoso Aranda <37244658+Andre-Reinoso@users.noreply.github.com>
2019-10-01 15:26:41 -0500

    login ejercicio3
    login ejercicio3

---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Controllers/Ejercicio3Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrabajoFinal_U1_WebII.Models;
+
+namespace TrabajoFinal_U1_WebII.Controllers
+{
+    public class Ejercicio3Controller : Controller
+    {
+        // GET: Ejercicio3
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoginUsuario()
+        {
+            ClsEjercicio3 objUsuarioLogin = new ClsEjercicio3();
+            if (Request.Form["password"]!= "" || Request.Form["usuario"] != "" || Request.Form["password"] != null || Request.Form[""] != null)
+            {
+                string usuario = Request.Form["usuario"];
+                string password = Request.Form["password"];
+
+                ClsEjercicio3 objEjer3 = new ClsEjercicio3();    
+                    objUsuarioLogin = objEjer3.LoginUsuario(usuario, password);
+            }
+
+
+            return View(objUsuarioLogin);
+        }
+    }
+}
---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Models/ClsEjercicio3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Librerias agregadas
+using System.Data;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace TrabajoFinal_U1_WebII.Models
+{
+    public class ClsEjercicio3
+    {
+        public int id { get; set; }
+        public string usuario { get; set; }
+        public string password { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public double dinero { get; set; }
+        public bool estado { get; set; }
+        public string tipoCuenta { get; set; }
+
+
+        public ClsEjercicio3 LoginUsuario(string usuarioLog, string passwordLog)
+        {
+            XDocument xmlUsuario = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/clientes.xml"));
+            var objEjercicio3 = new ClsEjercicio3();
+            objEjercicio3 = (from c in xmlUsuario.Descendants("cliente")
+                             where c.Element("usuario").Value.ToString() == (usuarioLog) ||
+                                   c.Element("password").Value.ToString() == (passwordLog)
+                             select new ClsEjercicio3
+                             {
+                                 id = Convert.ToInt32(c.Element("id").Value.ToString()),
+                                 usuario = c.Element("usuario").Value.ToString(),
+                                 password = c.Element("password").Value.ToString(),
+                                 nombre = c.Element("nombre").Value.ToString(),
+                                 apellido = c.Element("apellido").Value.ToString(),
+                                 dinero = Convert.ToDouble(c.Element("dinero").Value.ToString()),
+                                 tipoCuenta = c.Element("tipocuenta").Value.ToString(),
+                                 estado = Convert.ToBoolean(c.Element("estado").Value.ToString())
+
+                             }
+                             ).First();
+
+
+            return objEjercicio3;
+        }
+    }
+}
