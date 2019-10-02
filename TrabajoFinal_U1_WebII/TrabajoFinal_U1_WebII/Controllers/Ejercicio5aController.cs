76b968809b121cb3d8ecc389d238f66a0a604594
Andre Reinoso Aranda <37244658+Andre-Reinoso@users.noreply.github.com>
2019-10-02 14:46:32 -0500

    ejercicio5a terminado
    ejercicio5a terminado

---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Controllers/Ejercicio5aController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Data;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using TrabajoFinal_U1_WebII.Models;
+using System.Web.Script.Serialization;
+
+namespace TrabajoFinal_U1_WebII.Controllers
+{
+    public class Ejercicio5aController : Controller
+    {
+        // GET: Ejercicio5a
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult MostrarBusqueda()
+        {
+            ClsEjercicio5a objEjer5a = new ClsEjercicio5a();
+
+            if (Request.Form["tipoCuenta"] !=null)
+            {
+                var resultado = objEjer5a.BuscarUsuario(Request.Form["tipoCuenta"]);
+                resultado.Cast<object>().ToArray();
+                return Json(resultado, JsonRequestBehavior.AllowGet);
+
+            }
+            else
+            {
+                var resultado = objEjer5a.BuscarUsuario(Request.Form["tipoCuenta"]);
+                resultado.Cast<object>().ToArray();
+                return Json(resultado, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Models/ClsEjercicio5a.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using TrabajoFinal_U1_WebII.Models;
+
+namespace TrabajoFinal_U1_WebII.Models
+{
+    public class ClsEjercicio5a
+    {
+
+        public List<ClsEjercicio3> BuscarUsuario(string tipoCuenta)
+        {
+            XDocument xmlUsuario = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/clientes.xml"));
+            var objEjer = new List<ClsEjercicio3>();
+            objEjer = (from c in xmlUsuario.Descendants("cliente")
+                             where c.Element("tipocuenta").Value.ToString() == (tipoCuenta)
+                             select new ClsEjercicio3
+                             {
+                                 id = Convert.ToInt32(c.Element("id").Value.ToString()),
+                                 usuario = c.Element("usuario").Value.ToString(),
+                                 password = c.Element("password").Value.ToString(),
+                                 nombre = c.Element("nombre").Value.ToString(),
+                                 apellido = c.Element("apellido").Value.ToString(),
+                                 dinero = Convert.ToDouble(c.Element("dinero").Value.ToString()),
+                                 tipoCuenta = c.Element("tipocuenta").Value.ToString(),
+                                 estado = Convert.ToBoolean(c.Element("estado").Value.ToString())
+
+                             }
+                             ).ToList();
+            return objEjer;
+        }
+
+    }
+}
