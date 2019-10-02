a18870bb4595386821ffb9d1aa529800cdcfcfd2
Andre Reinoso Aranda <37244658+Andre-Reinoso@users.noreply.github.com>
2019-10-02 12:03:06 -0500

    ejercicio 6c
    ejercicio 6c

---
+++ TrabajoFinal_U1_WebII/TrabajoFinal_U1_WebII/Controllers/Ejercicio6cController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabajoFinal_U1_WebII.Controllers
+{
+    public class Ejercicio6cController : Controller
+    {
+        // GET: Ejercicio6c
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
